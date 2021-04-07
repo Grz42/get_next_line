59801176ef9d5ed9867aac749b8f80600720f866
Grz42 <egrazina@student.42lisboa.com>
2021-04-07 15:42:03 +0100

    Add files via upload

---
+++ get_next_line_bonus.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egrazina <egrazina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 11:20:55 by egrazina          #+#    #+#             */
+/*   Updated: 2021/04/06 18:35:57 by egrazina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+	size_t	length;
+
+	if (!src)
+		return (0);
+	length = ft_strlen(src);
+	if (!dst || !size)
+		return (length);
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (length);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	readfile(int fd, char **arr, char *buffer)
+{
+	ssize_t	rb;
+
+	rb = read(fd, buffer, BUFFER_SIZE);
+	if (rb < 0)
+		return (-1);
+	if (rb == 0)
+		return (0);
+	buffer[rb] = '\0';
+	if (arr[fd] == NULL)
+		arr[fd] = ft_strdup(buffer);
+	else
+		arr[fd] = ft_strjoin(arr[fd], buffer);
+	if (arr[fd] == NULL)
+		return (-1);
+	return (1);
+}
+
+int	update_line(int fd, char **arr, char **line)
+{
+	char	*index;
+	char	*tmp;
+
+	index = ft_strchr(arr[fd], '\n');
+	if (index)
+	{
+		*line = ft_substr(arr[fd], 0, index - arr[fd]);
+		tmp = ft_substr(index, 1, ft_strlen(arr[fd]) - (index - arr[fd] + 1));
+		free(arr[fd]);
+		arr[fd] = tmp;
+		return (1);
+	}
+	else
+	{
+		if (arr[fd])
+		{
+			*line = ft_strdup(arr[fd]);
+			free(arr[fd]);
+			arr[fd] = NULL;
+		}
+		else
+			*line = ft_strdup("\0");
+	}
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*arr[OPEN_MAX];
+	int			status;
+	char		*buffer;
+
+	status = 1;
+	if (line == NULL || BUFFER_SIZE <= 0 || fd < 0 || fd >= OPEN_MAX)
+		return (-1);
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (-1);
+	while (!ft_strchr(arr[fd], '\n') && status > 0)
+		status = readfile(fd, arr, buffer);
+	free(buffer);
+	if (status == -1)
+		return (-1);
+	else
+		status = update_line(fd, arr, line);
+	return (status);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egrazina <egrazina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 11:45:12 by egrazina          #+#    #+#             */
+/*   Updated: 2021/04/06 18:36:04 by egrazina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (&((char *)s)[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return (&((char *)s)[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*newstr;
+	size_t	l;
+
+	l = ft_strlen(s);
+	newstr = malloc(sizeof(char) * l + 1);
+	if (!newstr)
+		return (NULL);
+	ft_strlcpy(newstr, s, l + 1);
+	return (newstr);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sub_str;
+
+	if (!s)
+		return (NULL);
+	sub_str = malloc(sizeof(char) * len + 1);
+	if (!sub_str)
+		return (NULL);
+	if (start < ft_strlen(s))
+	{
+		ft_strlcpy(sub_str, &s[start], len + 1);
+		return (sub_str);
+	}
+	else
+		sub_str[0] = '\0';
+	return (sub_str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	l1;
+	size_t	l2;
+	char	*arr;
+
+	if (!s1 && !s2)
+		return (0);
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	arr = malloc(sizeof(char) * (l1 + l2) + 1);
+	if (!arr)
+		return (NULL);
+	ft_strlcpy(arr, s1, l1 + 1);
+	ft_strlcpy(&arr[l1], s2, l2 + 1);
+	free(s1);
+	return (arr);
+}
